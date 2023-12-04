c774abfece446f2671bbf4568c618b2c93452be9
bcat95 <bcat9505@gmail.com>
2023-12-04 22:12:23 +0700

    del

--- src/app/hooks/use-user-config.ts
+++
@@ -1,7 +0,0 @@
-import useSWRImmutable from 'swr/immutable'
-import { getUserConfig } from '~services/user-config'
-
-export function useUserConfig() {
-  const { data } = useSWRImmutable('user-config', getUserConfig, { suspense: true })
-  return data
-}
